6290af681524bec302f7cc6d6a928a73ca0a4e59
Hope-Cube <horvathhunor1@gmail.com>
2023-12-15 08:47:08 +0100

    Less rows.

--- AStarPath/AStar.cs
+++ AStarPath/AStar.cs
@@ -9,9 +9,7 @@ namespace AStarPath
         {
             List<AStarNode> openSet = new List<AStarNode>();
             HashSet<AStarNode> closedSet = new HashSet<AStarNode>();
-
             openSet.Add(startNode);
-
             while (openSet.Count > 0)
             {
                 AStarNode currentNode = openSet[0];
@@ -22,29 +20,24 @@ namespace AStarPath
                         currentNode = openSet[i];
                     }
                 }
-
                 openSet.Remove(currentNode);
                 closedSet.Add(currentNode);
-
                 if (currentNode == endNode)
                 {
                     return RetracePath(startNode, endNode);
                 }
-
                 foreach (AStarNode neighbor in currentNode.Neighbors)
                 {
                     if (neighbor.IsObstacle || closedSet.Contains(neighbor))
                     {
                         continue;
                     }
-
                     int newCostToNeighbor = currentNode.GCost + GetDistance(currentNode, neighbor);
                     if (newCostToNeighbor < neighbor.GCost || !openSet.Contains(neighbor))
                     {
                         neighbor.GCost = newCostToNeighbor;
                         neighbor.HCost = GetDistance(neighbor, endNode);
                         neighbor.Parent = currentNode;
-
                         if (!openSet.Contains(neighbor))
                         {
                             openSet.Add(neighbor);
@@ -52,11 +45,9 @@ namespace AStarPath
                     }
                 }
             }
-
             // Path not found
             return null;
         }
-
         private static List<AStarNode> RetracePath(AStarNode startNode, AStarNode endNode)
         {
             List<AStarNode> path = new List<AStarNode>();
@@ -71,12 +62,10 @@ namespace AStarPath
             path.Reverse();
             return path;
         }
-
         private static int GetDistance(AStarNode nodeA, AStarNode nodeB)
         {
             int distX = Math.Abs(nodeA.X - nodeB.X);
             int distY = Math.Abs(nodeA.Y - nodeB.Y);
-
             return distX + distY;
         }
     }
--- AStarPath/AStarNode.cs
+++ AStarPath/AStarNode.cs
@@ -12,7 +12,6 @@ namespace AStarPath
         public int GCost { get; set; }
         public int HCost { get; set; }
         public int FCost => GCost + HCost;
-
         public AStarNode(int x, int y, bool isObstacle = false)
         {
             X = x;
@@ -24,19 +23,15 @@ namespace AStarPath
         public void GenerateNeighbors(AStarNode[,] grid)
         {
             Neighbors.Clear();
-
             int gridSizeX = grid.GetLength(0);
             int gridSizeY = grid.GetLength(1);
-
             // Define the possible offsets for adjacent nodes (up, down, left, right)
             int[] xOffset = { 0, 0, -1, 1 };
             int[] yOffset = { -1, 1, 0, 0 };
-
             for (int i = 0; i < xOffset.Length; i++)
             {
                 int checkX = X + xOffset[i];
                 int checkY = Y + yOffset[i];
-
                 if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
                 {
                     Neighbors.Add(grid[checkX, checkY]);
@@ -47,20 +42,16 @@ namespace AStarPath
         /*public void GenerateNeighbors(AStarNode[,] grid)
         {
             Neighbors.Clear();
-
             int gridSizeX = grid.GetLength(0);
             int gridSizeY = grid.GetLength(1);
-
             for (int x = -1; x <= 1; x++)
             {
                 for (int y = -1; y <= 1; y++)
                 {
                     if (x == 0 && y == 0)
                         continue;
-
                     int checkX = X + x;
                     int checkY = Y + y;
-
                     if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
                     {
                         Neighbors.Add(grid[checkX, checkY]);
