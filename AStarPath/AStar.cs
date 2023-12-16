0a70b74d287ec29d411ca3feba00962dec3219e6
Hope-Cube <hopecube@gmail.com>
2023-12-16 21:50:03 +0100

    AStarPath is done.

--- AStarPath/AStar.cs
+++ AStarPath/AStar.cs
@@ -1,10 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using static System.Console;
 
 namespace AStarPath
 {
     internal class AStar
     {
+        public static void Path(Point identifier, Point sp, Point ep)
+        {
+            // Create a 2D array to represent the grid
+            AStarNode[,] grid = new AStarNode[11, 11];
+            // Initialize the grid with nodes
+            for (int x = 0; x < 11; x++) for (int y = 0; y < 11; y++) grid[x, y] = new AStarNode(x, y);
+            // Create point list
+            List<Point> points = new List<Point>();
+            int[] id = GetField(identifier);
+            using (Bitmap field = new Bitmap("field.png")) for (int y = 0; y < 11; y++) for (int x = 0; x < 11; x++) if (field.GetPixel((id[1] * 11) + x, (id[0] * 11) + y) == Color.FromArgb(0, 0, 0)) points.Add(new Point(x, y));
+            // Mark obstacles in the grid (for example, obstacle at (5,5))
+            foreach (Point point in points) grid[point.X, point.Y].IsObstacle = true;
+            // Generate neighbors for each node
+            for (int x = 0; x < 11; x++) for (int y = 0; y < 11; y++) grid[x, y].GenerateNeighbors(grid);
+            // Define the start and end nodes
+            AStarNode startNode = grid[GetIndexByNumber(sp.X), GetIndexByNumber(sp.Y)];
+            AStarNode endNode = grid[GetIndexByNumber(ep.X), GetIndexByNumber(ep.Y)];
+            // Find the path using A* algorithm
+            List<AStarNode> path = FindPath(startNode, endNode);
+            // Print the path
+            if (path != null)
+            {
+                WriteLine("Path found:");
+                Bitmap pathBitmap = new Bitmap(11, 11);
+                for (int i = 0; i < path.Count - 1; i += 2)
+                {
+                    AStarNode currentNode = path[i];
+                    AStarNode nextNode = (i + 1 < path.Count) ? path[i + 1] : null;
+                    int dx = (nextNode != null) ? nextNode.X - currentNode.X : 0;
+                    int dy = (nextNode != null) ? nextNode.Y - currentNode.Y : 0;
+                    string direction = GetDirection(dx, dy);
+                    WriteLine($"{direction}");
+                    pathBitmap.SetPixel(currentNode.X, currentNode.Y, Color.FromArgb(0, 255, 255));
+                    if (nextNode != null) pathBitmap.SetPixel(nextNode.X, nextNode.Y, Color.FromArgb(0, 255, 255));
+                }
+                // Add the last node to the bitmap
+                pathBitmap.SetPixel(path.Last().X, path.Last().Y, Color.FromArgb(255, 0, 0));
+                path.Insert(0, startNode);
+                pathBitmap.SetPixel(path.First().X, path.First().Y, Color.FromArgb(0, 255, 0));
+                pathBitmap.Save("path.png");
+            }
+            else WriteLine("No path found.");
+        }
+        private static int[] GetField(Point idp)
+        {
+            return
+                (idp == new Point(0, 1) || idp == new Point(5, 2)) ? new int[] { 0, 0 } :
+                (idp == new Point(1, 3) || idp == new Point(4, 1)) ? new int[] { 1, 0 } :
+                (idp == new Point(3, 3) || idp == new Point(5, 3)) ? new int[] { 2, 0 } :
+                (idp == new Point(0, 0) || idp == new Point(3, 0)) ? new int[] { 1, 0 } :
+                (idp == new Point(3, 5) || idp == new Point(4, 2)) ? new int[] { 1, 1 } :
+                (idp == new Point(2, 4) || idp == new Point(4, 0)) ? new int[] { 1, 2 } :
+                (idp == new Point(1, 0) || idp == new Point(5, 1)) ? new int[] { 2, 0 } :
+                (idp == new Point(2, 3) || idp == new Point(3, 0)) ? new int[] { 2, 1 } :
+                (idp == new Point(0, 4) || idp == new Point(2, 1)) ? new int[] { 2, 2 } :
+                new int[] { };
+        }
+        // Helper method to get the direction based on dx and dy
+        private static string GetDirection(int dx, int dy)
+        {
+            return
+                (dx == 1) ? "right" :
+                (dx == -1) ? "left" :
+                (dy == 1) ? "down" :
+                (dy == -1) ? "up" :
+                "unknown";
+        }
+        private static int GetIndexByNumber(int number)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            return
+                (number >= 1 && number <= 6) ? array[(number - 1) * 2] - 1 :
+                -1;
+        }
         public static List<AStarNode> FindPath(AStarNode startNode, AStarNode endNode)
         {
             List<AStarNode> openSet = new List<AStarNode>();
--- AStarPath/Program.cs
+++ AStarPath/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using static System.Console;
@@ -9,100 +10,11 @@ namespace AStarPath
     {
         static void Main()
         {
-            // Create a 2D array to represent the grid
-            AStarNode[,] grid = new AStarNode[11, 11];
-            // Initialize the grid with nodes
-            for (int x = 0; x < 11; x++)
-            {
-                for (int y = 0; y < 11; y++)
-                {
-                    grid[x, y] = new AStarNode(x, y);
-                }
-            }
-            // Create point list
-            List<Point> points = new List<Point>();
-            using (Bitmap field = new Bitmap("field.png"))
-            {
-                for (int y = 0; y < field.Height; y++)
-                {
-                    for (int x = 0; x < field.Width; x++)
-                    {
-                        if (field.GetPixel(y, x) == Color.FromArgb(0, 0, 0))
-                        {
-                            points.Add(new Point(y, x));
-                        }
-                    }
-                }
-            }
-            // Mark obstacles in the grid (for example, obstacle at (5,5))
-            foreach (Point point in points)
-            {
-                grid[point.X, point.Y].IsObstacle = true;
-            }
-            // Generate neighbors for each node
-            for (int x = 0; x < 11; x++)
-            {
-                for (int y = 0; y < 11; y++)
-                {
-                    grid[x, y].GenerateNeighbors(grid);
-                }
-            }
-            // Define the start and end nodes
-            WriteLine("Set the start and end points' coords! (6,0,0,2)");
-            int[] coords = ReadLine().Split(',').Select(int.Parse).ToArray();
-            AStarNode startNode = grid[coords[0], coords[1]];
-            AStarNode endNode = grid[coords[2], coords[3]];
-            // Find the path using A* algorithm
-            List<AStarNode> path = AStar.FindPath(startNode, endNode);            
-            // Print the path
-            if (path != null)
-            {
-                WriteLine("Path found:");
-                Bitmap pathBitmap = new Bitmap(11, 11);
-                for (int i = 0; i < path.Count - 1; i += 2)
-                {
-                    AStarNode currentNode = path[i];
-                    AStarNode nextNode = (i + 1 < path.Count) ? path[i + 1] : null;
-                    int dx = (nextNode != null) ? nextNode.X - currentNode.X : 0;
-                    int dy = (nextNode != null) ? nextNode.Y - currentNode.Y : 0;
-                    string direction = GetDirection(dx, dy);
-                    WriteLine($"{direction}");
-                    pathBitmap.SetPixel(currentNode.X, currentNode.Y, Color.FromArgb(0, 255, 255));
-                    if (nextNode != null)
-                    {
-                        pathBitmap.SetPixel(nextNode.X, nextNode.Y, Color.FromArgb(0, 255, 255));
-                    }
-                }
-                // Add the last node to the bitmap
-                pathBitmap.SetPixel(path.Last().X, path.Last().Y, Color.FromArgb(0, 255, 255));
-                pathBitmap.Save("path.png");
-            }
-            else
-            {
-                WriteLine("No path found.");
-            }
+            WriteLine("Give the id, sp and ep. (0,0,4,6,5,3)");
+            int[] ints = ReadLine().Split(',').Select(int.Parse).ToArray();
+            Clear();
+            AStar.Path(new Point(ints[0], ints[1]), new Point(ints[2], ints[3]), new Point(ints[4], ints[5]));
             ReadKey(true);
-        }
-        // Helper method to get the direction based on dx and dy
-        private static string GetDirection(int dx, int dy)
-        {
-            if (dx == 1)
-            {
-                return "right";
-            }
-            else if (dx == -1)
-            {
-                return "left";
-            }
-            else if (dy == 1)
-            {
-                return "down";
-            }
-            else if (dy == -1)
-            {
-                return "up";
-            }
-            return "unknown";
         }
     }
 }
